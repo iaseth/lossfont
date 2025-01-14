9cb25688573c7d0e0d1cde89159ffd6ee8c1f768
Ankur Seth <iankurseth@gmail.com>
2025-01-14 21:41:48 +0530

    Removed boilerplate

--- react-app/src/App/screens/MainScreen/MainScreen.tsx
+++ react-app/src/App/screens/MainScreen/MainScreen.tsx
@@ -1,24 +1,14 @@
-import { RedIcon } from "redicons";
-import { Footer, Header } from "@/components";
 
 
 
 export default function MainScreen () {
 	return (
 		<div>
-			<Header />
-
-			<main className="min-h-screen px-4 py-20">
-				<section className="">
-					<a href="https://github.com/iaseth/vite-react-ts-starter" className="block w-64 m-auto bg-zinc-800 text-zinc-200 text-center px-8 py-6 rounded">
-						<RedIcon name="bs-foo" className="mx-auto" size={40} />
-						<h1 className="H4">Starter</h1>
-						<h4>vite-react-ts-starter</h4>
-					</a>
+			<main className="min-h-screen px-4 py-20 flex">
+				<section className="m-auto">
+					<h4>Main</h4>
 				</section>
 			</main>
-
-			<Footer />
 		</div>
 	);
 }
